2e17807d7cc8a9bb97320e2566e409ff9fa83db5
George <sonicgd@gmail.com>
2019-05-05 13:38:01 +0500

    Refactor model inheritance

--- src/BioEngine.Core.API/Entities/Tag.cs
+++ src/BioEngine.Core.API/Entities/Tag.cs
@@ -6,19 +6,19 @@ namespace BioEngine.Core.API.Entities
     public class Tag : RestModel<Core.Entities.Tag>, IRequestRestModel<Core.Entities.Tag>,
         IResponseRestModel<Core.Entities.Tag>
     {
-        public string Name { get; set; }
+        public string Title { get; set; }
 
         public async Task<Core.Entities.Tag> GetEntityAsync(Core.Entities.Tag entity)
         {
             entity = await FillEntityAsync(entity);
-            entity.Name = Name;
+            entity.Title = Title;
             return entity;
         }
 
         public async Task SetEntityAsync(Core.Entities.Tag entity)
         {
             await ParseEntityAsync(entity);
-            Name = entity.Name;
+            Title = entity.Title;
         }
     }
 }
