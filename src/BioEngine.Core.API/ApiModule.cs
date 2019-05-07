e9b7a9cb7d928978961e05aed1eed932054493c5
George <sonicgd@gmail.com>
2019-05-07 22:43:16 +0500

    BioEngine 3.0 must run on ASP.NET Core 3.0 =)

--- src/BioEngine.Core.API/ApiModule.cs
+++ src/BioEngine.Core.API/ApiModule.cs
@@ -2,15 +2,15 @@ using System.Collections.Generic;
 using System.Reflection;
 using BioEngine.Core.API.Models;
 using BioEngine.Core.Modules;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.Core.API
 {
     public class ApiModule : BioEngineModule<ApiModuleConfig>
     {
-        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration, IHostingEnvironment environment)
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
         {
             var assembliesList = new List<Assembly>(Config.Assemblies)
                 {typeof(ApiModule).Assembly};
