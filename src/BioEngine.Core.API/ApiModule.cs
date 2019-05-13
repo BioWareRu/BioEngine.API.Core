f131f2d6e7936bada28382021407483ae637783e
George <sonicgd@gmail.com>
2019-05-13 17:30:23 +0500

    Remove hardcoded env configuration from modules. Remove duplicated config classes.

--- src/BioEngine.Core.API/ApiModule.cs
+++ src/BioEngine.Core.API/ApiModule.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.Core.API
 {
-    public class ApiModule : WebModule
+    public class ApiModule : WebModule<ApiModuleConfig>
     {
         public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
             IHostEnvironment environment)
@@ -26,5 +26,9 @@ namespace BioEngine.Core.API
                         classes.AssignableToAny(typeof(IResponseRestModel<>), typeof(IRequestRestModel<>)))
                     .AsSelf());
         }
+    }
+
+    public class ApiModuleConfig : WebModuleConfig
+    {
     }
 }
